1e710b25d92a879cea777aef0cb4fe5d2a1b21e5
Mark Terris <terris_mark@mail.ru>
2020-12-11 21:29:22 +0400

    fised a little bug

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://todoman-app.herokuapp.com"
-export const API_URL = "http://localhost:5000"
+export const API_URL = "https://todoman-app.herokuapp.com"
+// export const API_URL = "http://localhost:5000"
